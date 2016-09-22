009f09de5f8390341a20b32934ac4f009c8c1371
Megaju <julien_malle@outlook.fr>
2016-09-22 17:14:33 +0200

    modif phrase de connection

--- adminspace.php
+++ adminspace.php
@@ -2,7 +2,7 @@
 <?php include('includes/header.php'); ?>
     <section class="motdepasse">
     <h2>PAGE PROTÉGÉ PAR MOT DE PASSE</h2>
-    <p>Si tu es moi tu connais le mot de passe, sinon merci de passer ton chemin, bye, passe un jour à la maison y a des p'tit gâteau des p'tits anges tout nus du coca et d'la vodka tagada.</p>
+    <p>Si tu es moi tu connais le mot de passe, sinon merci de passer ton chemin, bye, passe un jour à la maison y a des p'tits gâteaux, des p'tits fours, des p'tits anges tout et d'la vodka tagada. :)</p>
     <!-- demande de mot de passe -->
     <form action="write_news.php" method="POST">
         <input type="password" name="mdp">
@@ -25,4 +25,5 @@
             overflow-x: hidden;
         }
     </style>
+    
 <?php include('includes/footer.php'); ?>
