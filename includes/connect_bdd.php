0ac7cbcf7ee890bc09b422e9d32bbdd32b17d59f
Megaju <julen_malle@outlook.fr>
2016-09-22 20:33:57 +0200

    supression de DBSingleton et ajout d'une nouvelle connection locale pour la maison

--- includes/connect_bdd.php
+++ includes/connect_bdd.php
@@ -17,7 +17,8 @@ class DBSingleton
         return self::$bdd;
     }
     private function __construct(){
-     self::$bdd = new PDO('mysql:host=localhost;dbname=DB_DEATHNOTE;charset=utf8', 'root', 'facesimplon');
+     //self::$bdd = new PDO('mysql:host=localhost;dbname=DB_DEATHNOTE;charset=utf8', 'root', 'facesimplon');
+     self::$bdd = new PDO('mysql:host=localhost;dbname=DB_DEATHNOTE;charset=utf8', 'root', 'iamj');
     }
 }
 
